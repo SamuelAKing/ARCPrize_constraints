3309520a86712b75941cac89844e7e4fc7c14b3b
Sammify <76973317+Sammify@users.noreply.github.com>
2025-06-11 17:29:28 -0400

    Add existing file

---
+++ constraints.py
@@ -0,0 +1,172 @@
+from definitions import *
+
+def puzzle_16b78196(input,output):
+
+    #Definition: All shapes are considered to consist of only one colour (not black) and to be of connectivity 4.
+    input_shapes = []
+    output_shapes = []
+    for color in COLORS:
+        if color != COLOR_mapping['black']:
+            [input_shapes.append(shape) for shape in find_shapes(input,4,[color])]
+            [output_shapes.append(shape) for shape in find_shapes(output,4,[color])]
+
+    #Definition: The divider is the shape that contains a full column or a full row. If it contains a full row, it is horizontal, and if it contains a full column, it is vertical.
+    def divider(shapes):
+        #the divider is a list containing a shape and a string from ['horizontal','vertical'] that states whether the divider is horizontal or vertical
+        for shape in shapes:
+            if True in [-1 not in row for row in shape.grid]:
+                return [shape,'horizontal']
+            if True in [-1 not in row for row in shape.rotate(1).grid]:
+                return [shape,'vertical']
+        return [shapes[0],None]
+    input_divider = divider(input_shapes)
+    output_divider = divider(output_shapes)
+    
+    constraints = []
+
+    #Constraint: The output is of the same dimensions as the input.
+    constraints.append(len(input)==len(output) and len(input[0])==len(output[0]))
+
+    #Constraint: The input and output dividers are exactly equal.
+    constraints.append(input_divider[0].equals(output_divider[0]))
+        
+    #Constraint: Each shape in the input is translationally equal to a shape in the output.
+    constraints.append(False not in [True in [input_shape.translationally_equals(output_shape) for output_shape in output_shapes] for input_shape in input_shapes])
+    
+    #Constraint: There is the same number of shapes in the input and in the output.
+    constraints.append(len(input_shapes)==len(output_shapes))
+    
+    #Used for the following two constraints
+    def num_partitions(lst, splitter):
+        count = 0
+        segment = False
+        for item in lst:
+            if item == splitter:
+                if segment:
+                    count += 1
+                segment = False
+            else:
+                segment = True
+        if segment:
+            count += 1
+        return count
+
+    #Constraint: In the output, if the divider is vertical, in all rows, there are no black pixels between two non-black pixels.
+    if output_divider[1] == 'vertical':
+        constraints.append(False not in [num_partitions(row,0)<=1 for row in output])
+    else:
+        constraints.append(True)
+
+    #Constraint: In the output, if the divider is horizontal, in all columns, there are no black pixels between two non-black pixels.
+    if output_divider[1] == 'horizontal':
+        constraints.append(False not in [num_partitions(column,0)<=1 for column in [[output[k][i] for k in range(len(output))]for i in range(len(output[0]))]])
+    else:
+        constraints.append(True)
+
+    return False not in constraints
+
+def puzzle_6e453dd6(input,output):
+    constraints = []
+
+    #Constraint: The output is of the same dimensions as the input.
+    constraints.append(len(input)==len(output) and len(input[0])==len(output[0]))
+
+    #Constraint: All grey pixels in the input are grey in the output
+    constraints.append(True not in [True in [input[i][k] == COLOR_mapping['gray'] and output[i][k] != COLOR_mapping['gray'] for k in range(len(input[0]))] for i in range(len(input))])
+
+    #Constraint: Each black shape of connectivity 4 in the output is translationally equal to a black shape in the input, occupying exactly the same rows, and it has at least one pixel directly to the left of a grey pixel.
+    input_black_shapes = find_shapes(input,4,[COLOR_mapping['black']])
+    output_black_shapes = find_shapes(output,4,[COLOR_mapping['black']])
+
+    tests = []
+    for output_black_shape in output_black_shapes:
+        is_correct = False
+        for input_black_shape in input_black_shapes:
+            if output_black_shape.translationally_equals(input_black_shape) and [True in [pixel!=-1 for pixel in row] for row in output_black_shape.grid] == [True in [pixel!=-1 for pixel in row] for row in input_black_shape.grid]:
+                for i in range(len(input)):
+                    for k in range(len(input[0])):
+                        if output_black_shape.grid[i][k]==COLOR_mapping['black'] and output[i][k+1]==COLOR_mapping['gray']:
+                            is_correct = True
+        tests.append(is_correct)
+    constraints.append(False not in tests)
+
+    #Constraint: The input and output have the same number of black shapes of connectivity 4.
+    constraints.append(len(input_black_shapes)==len(output_black_shapes))
+
+    #Constraint: In the output, each pixel is red if and only if it is to the left of a grey pixel and it is in the same row as a pixel that has a grey pixel two spaces to the right and that is in a pink shape of connectivity 4 that is orthogonally adjacent to a black shape of connectivity 4 and whose pixels are all orthogonally adjacent to four pixels that are either black or pink.
+    output_pink_shapes = find_shapes(output,4,[COLOR_mapping['pink']])
+    pink_shapes_fitting_criteria = []
+    padded_output = [[output[i][k] if (i in range(len(output)) and k in range(len(output[0]))) else -1 for k in range(-1,len(output[0])+1)]for i in range(-1,len(output)+1)]
+    for output_black_shape in output_black_shapes:
+        adjacent_pink_shapes = []
+        for pink_shape in output_pink_shapes:
+            for i in range(1,len(input)-1):
+                for k in range(1,len(input[0])-1):
+                    if pink_shape not in adjacent_pink_shapes:
+                        if pink_shape.grid[i][k] != -1:
+                            if True in [output_black_shape.grid[i+neighbor[0]][k+neighbor[1]] != -1 for neighbor in [[0,1],[1,0],[0,-1],[-1,0]]]:
+                               adjacent_pink_shapes.append(pink_shape)
+        for adjacent_pink_shape in adjacent_pink_shapes:
+            keep_shape = True
+            for i in range(len(input)):
+                for k in range(len(input[0])):
+                    if keep_shape:
+                        if adjacent_pink_shape.grid[i][k] != -1:
+                            if False in [padded_output[i+neighbor[0]+1][k+neighbor[1]+1] in [COLOR_mapping['black'],COLOR_mapping['pink']] for neighbor in [[0,1],[1,0],[0,-1],[-1,0]]]:
+                                keep_shape = False
+            if keep_shape:
+                pink_shapes_fitting_criteria.append(adjacent_pink_shape)
+    rows_with_red = []
+    for pink_shape_fitting_criteria in pink_shapes_fitting_criteria:
+        for row_idx in range(len(pink_shape_fitting_criteria.grid)):
+            if row_idx not in rows_with_red:
+                for pixel_idx in range(len(pink_shape_fitting_criteria.grid[row_idx])-2):
+                    if pink_shape_fitting_criteria.grid[row_idx][pixel_idx] != -1:
+                        if output[row_idx][pixel_idx+2] == COLOR_mapping['gray']:
+                            rows_with_red.append(row_idx)
+                            if row_idx == 0:
+                                i = row_idx
+                                k = pixel_idx
+                                # print([padded_output[i+neighbor[0]+1][k+neighbor[1]+1] in [COLOR_mapping['black'],COLOR_mapping['pink']] for neighbor in [[0,1],[1,0],[0,-1],[-1,0]]])
+    tests = []
+    for i in range(len(input)):
+        for k in range(len(input[0])):
+            tests.append((COLOR_mapping['gray'] in output[i][0:k] and i in rows_with_red) is (output[i][k] == COLOR_mapping['red']))
+    constraints.append(False not in tests)
+    # print(tests,rows_with_red)
+
+    #Constraint: In the output, each pixel is pink, unless it is grey in the input, it is red, or it is black.
+    is_correct = True
+    for i in range(len(input)):
+        for k in range(len(input[0])):
+            if input[i][k] != COLOR_mapping['gray'] and output[i][k] != COLOR_mapping['red'] and output[i][k] != COLOR_mapping['black'] and output[i][k] != COLOR_mapping['pink']:
+                is_correct = False
+    constraints.append(is_correct)
+
+    # print(constraints)
+
+    return False not in constraints
+
+# print(test_constraints(puzzle_16b78196,"16b78196",0.001,10000)) returned True
+print(test_constraints(puzzle_6e453dd6,"6e453dd6",0.001,10000,2))
+
+# puzzle_id = "6e453dd6"
+# file = open("evaluation/"+puzzle_id+".json")
+# data = json.load(file)
+# file.close()
+
+# example = data['train'][0]
+# exout = example['output']
+
+# case = [[[0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [0, 0, 6, 0, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 0, 6, 0, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 0, 6, 0, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 0, 6, 0, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 0, 0, 0, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6]], [[6, 6, 6, 6, 6, 0, 0, 0, 0, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 0, 0, 6, 0, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 2, 6, 2, 2], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 2, 2, 2, 2], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 2, 2, 2, 2], [6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 2, 2, 2, 2], [6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 0, 5, 2, 2, 2, 2], [6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 5, 6, 6, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6, 6]]]
+# input = case[0]
+# output = case[1]
+
+
+# for i in range(len(input)):
+#     for k in range(len(input[0])):
+#         if exout[i][k] != output[i][k]:
+#             print(i,k)
+#             print(exout[i][k],output[i][k])
+
+# print(puzzle_6e453dd6(input,output))
---
+++ definitions.py
@@ -0,0 +1,131 @@
+from common import find_connected_components
+import numpy as np
+
+COLORS = [0,1,2,3,4,5,6,7,8,9]
+
+COLOR_mapping = {'black':0,'blue':1,'red':2,'green':3,'yellow':4,'gray':5,'pink':6,'orange':7,'teal':8,'brown':9}
+
+class Shape:
+
+    """
+    Shape: a shape is a set of pixels (whose colors are drawn from a specified set of colors) that cannot be expressed as the union of two non-empty sets of pixels where no pixel from one set is adjacent to a pixel in the other set. 
+    Here, adjacency can either be defined by orthogonal adjacency, or orthogonal and diagonal adjacency. 
+    These will be written as a shape of connectivity 4 in the first case and a shape of connectivity 8 in the second case. 
+    """
+
+    def __init__(self,connectivity,colors,grid):
+        self.connectivity = connectivity #has a value of either 4 or 8
+        self.colors = colors #set of the integers representing the colors that make up the shape
+        self.grid = grid #a 2D array that has a value of -1 where there is no shape and an integer representing a color when a pixel is part of a shape
+
+    
+    def equals(self,shape):
+        #Two shapes are equal if all their pixels are equal.
+        return self.grid==shape.grid
+
+    def translationally_equals(self,shape):
+        #Two shapes are translationally equal if one can be obtained by changing the position of each of its pixels by the same amount.
+        rows_shape1 = [row for row in self.grid if [pixel for pixel in row if pixel!=-1]!=[]]
+        rows_shape2 = [row for row in shape.grid if [pixel for pixel in row if pixel!=-1]!=[]]
+
+        boxed_shape1 = [column for column in [[rows_shape1[k][i] for k in range(len(rows_shape1))]for i in range(len(rows_shape1[0]))] if [pixel for pixel in column if pixel!=-1]!=[]]
+        boxed_shape2 = [column for column in [[rows_shape2[k][i] for k in range(len(rows_shape2))]for i in range(len(rows_shape2[0]))] if [pixel for pixel in column if pixel!=-1]!=[]]
+
+        return boxed_shape1 == boxed_shape2
+    
+    def rotate(self,angle):
+        #Rotation angle is defined in quarter turns and goes counterclockwise
+        if angle%4 == 0:
+            return self
+        if angle%4 == 1:
+            return Shape(self.connectivity,self.colors,[[self.grid[len(self.grid)-k-1][i] for k in range(len(self.grid))] for i in range(len(self.grid[0]))])
+        if angle%4 == 2:
+            return self.rotate(1).rotate(1)
+        if angle%4 == 3:
+            return self.rotate(2).rotate(1)
+        
+    def rotationally_equals(self,shape):
+        #Two shapes are rotationally equal if one is translationally equal to the rotation of the other’s grid by either 0°, 90°, 180°, or 270°.
+        return False not in [self.rotate(angle).translationally_equals(shape) for angle in range(4)]
+
+
+    def distance(self,point1,point2):
+        #The distance between two pixels in a shape is the minimum number of steps between adjacent pixels in the shape needed to go from one pixel to the other.
+        padded_grid = [[self.grid[i][k] if (i in range(len(self.grid[0])) and k in range(len(self.grid))) else -1 for i in range(-1,len(self.grid[0])+1)]for k in range(-1,len(self.grid)+1)] #adds a row and colum of -1 on all sides
+        dist = 0
+        points = [point1]
+        points_seen = points
+        if self.connectivity == 4:
+            neighbors = [[0,1],[1,0],[0,-1],[-1,0]]
+        if self.connectivity == 4:
+            neighbors = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
+        while True:
+            if point2 in points:
+                return dist
+            new_points = []
+            for point in points:
+                for neighbor in neighbors:
+                        new_point = [x + y for x, y in zip(point, neighbor)]
+                        if padded_grid[new_point[0]+1][new_point[1]+1] != -1 and new_point not in new_points and new_point not in points_seen:
+                            new_points.append(new_point)
+            points_seen.append(new_points)
+            points = new_points
+            dist += 1
+
+def background(grid):
+    #Background: the background is the set of all pixels of the most common color.
+    flat_grid = [pixel for row in grid for pixel in row]
+    return [[pixel if pixel==max(set(flat_grid), key=flat_grid.count) else -1 for pixel in row] for row in grid]
+    
+def find_shapes(grid,connectivity,colors): #finds all the shapes in a given grid
+    binary_grid = [[pixel if pixel in colors else -1 for pixel in row] for row in grid] #turns all pixels that aren't the right color into -1
+    shapes = [Shape(connectivity,colors,shape.tolist()) for shape in find_connected_components(np.array(binary_grid),-1,connectivity,False)] #turns the numpy arrays into shapes
+    return shapes
+
+
+###########################
+### TESTING CONSTRAINTS ###
+###########################
+import random
+import json
+import time
+
+def test_constraints(puzzle_func,puzzle_id,p,iters,debug=0):
+    file = open("evaluation/"+puzzle_id+".json")
+    data = json.load(file)
+    file.close()
+
+    examples = []
+    for case in data['train']:
+        input = case['input']
+        output = case['output']
+        examples.append([input,output])
+    for case in data['test']:
+        input = case['input']
+        output = case['output']
+        examples.append([input,output])
+
+    tests = []
+    for input,output in examples:
+        tests.append(puzzle_func(input,output))
+    
+    for iter in range(iters):
+        for input,output in examples:
+            new_output = [[pixel if random.random()>p else COLORS[random.randint(0,9)] for pixel in row] for row in output]
+            tests.append(puzzle_func(input,new_output)==([input,new_output] in examples))
+            if debug==2:
+                if not (puzzle_func(input,new_output)==([input,new_output] in examples)):
+                    print([input,new_output],iter)
+
+    if debug==1:
+        print(tests)
+
+    return False not in tests
+
+def timer(func,inputs):
+    result = []
+    for inpt in inputs:
+        start = time.time_ns()
+        func(inpt)
+        result.append((time.time_ns()-start)*10**-9)
+    return result
